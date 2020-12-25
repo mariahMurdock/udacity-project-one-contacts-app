6a6bc95e24abfcf3a1aa171230159dd57d5a0745
mariahMurdock <mariahmurdock1227@gmail.com>
2020-12-25 08:50:07 -0800

    add up to date work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      { name: 'Tyler '},
+      { name: 'Karen '},
+      { name: 'Richard '},
+    ]
+
+    return <ol>
+      <people.map((person) => (
+        <li key={person.name}>{person.name}</li> 
+        ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">	      
+      <header className="App-header">	        
+        <img src={logo} className="App-logo" alt="logo" />	     
+        <h1 className="App-title">Welcome to React</h1>	          
+      </header>	          
+      <p className="App-intro">	        ]}/>
+        To get started, edit <code>src/App.js</code> and save to reload.	       
+      </p>	          
+    </div>	     
+    );
+  }
 }
 
 export default App;
+
+
+
+	class App extends Component {
+    render() {
+   	  return (
+       <div className="App">
+        <ContactList contacts={[
+         { name: 'Tyler '},
+        { name: 'Karen' },
+        { name: 'Richard '}
+        ]}/>
+       <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Mikenzi' },
+          { name: 'Ryan' }
+        ]}/>
+      </div>
+    );
+  }
